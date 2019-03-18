57a1ed01f11ac8c3ac866549907c55a9d7432f3c
Damon Clark <damon.clark69@gmail.com>
2019-03-18 16:00:10 +0200

    Fixing recursion error

--- testpackage1/recursion.py
+++ testpackage1/recursion.py
@@ -13,11 +13,11 @@ def sum_array(array):
 def fibonacci(n):
     '''Return nth term in fibonacci sequence'''
 
-    if n < 0:
+    if n+1 < 0:
         print("Incorrect input, please make sure the number is > 0")
-    elif n == 1:
+    elif n+1 == 1:
         return 0
-    elif n == 2:
+    elif n+1 == 2:
         return 1
     else:
         return fibonacci(n-1)+fibonacci(n-2)
